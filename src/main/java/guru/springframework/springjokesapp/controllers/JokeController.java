ccfdd81734aecea6ffd7fd5a2f66e809e2e16ceb
henriquewallace <henrique.wallace@hotmail.com>
2023-07-21 00:25:17 -0300

    create controller and get joke

---
+++ src/main/java/guru/springframework/springjokesapp/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package guru.springframework.springjokesapp.controllers;
+
+import guru.springframework.springjokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String getJoke(Model model) {
+        model.addAttribute("joke", jokeService.getRandomQuote());
+
+        return "joke/index";
+    }
+}
--- src/main/java/guru/springframework/springjokesapp/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/springjokesapp/services/JokeServiceImpl.java
@@ -1,13 +1,15 @@
 package guru.springframework.springjokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
-        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
 
     @Override
